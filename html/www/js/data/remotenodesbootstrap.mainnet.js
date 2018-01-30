423bfb059a172ac49b2204b181cc0ea1f4f005b4
root <root@nms.wifisystem.id>
2018-01-30 03:02:18 +0000

    Add HTML UI

---
+++ html/www/js/data/remotenodesbootstrap.mainnet.js
@@ -0,0 +1,97 @@
+/******************************************************************************
+ * Copyright © 2013-2016 The Nxt Core Developers.                             *
+ * Copyright © 2016 Jelurida IP B.V.                                          *
+ *                                                                            *
+ * See the LICENSE.txt file at the top-level directory of this distribution   *
+ * for licensing information.                                                 *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement with Jelurida B.V.,*
+ * no part of the Nxt software, including this file, may be copied, modified, *
+ * propagated, or distributed except according to the terms contained in the  *
+ * LICENSE.txt file.                                                          *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+RemoteNodesManager.prototype.REMOTE_NODES_BOOTSTRAP =
+    {
+        "peers": [
+            {
+                "downloadedVolume": 491153,
+                "address": "sg.apoacc.com",
+                "inbound": false,
+                "blockchainState": "UP_TO_DATE",
+                "weight": 0,
+                "uploadedVolume": 139210,
+                "services": [
+                    "API",
+                    "CORS"
+                ],
+                "version": "1.11.2",
+                "platform": "apo",
+                "inboundWebSocket": false,
+                "lastUpdated": 0,
+                "blacklisted": false,
+                "announcedAddress": "jp.apoacc.com",
+                "apiPort": 27876,
+                "application": "NRS",
+                "port": 27874,
+                "outboundWebSocket": true,
+                "lastConnectAttempt": 0,
+                "state": 1,
+                "shareAddress": true
+            },
+            {
+                "downloadedVolume": 491153,
+                "address": "de.apoacc.com",
+                "inbound": false,
+                "blockchainState": "UP_TO_DATE",
+                "weight": 0,
+                "uploadedVolume": 139210,
+                "services": [
+                    "API",
+                    "CORS"
+                ],
+                "version": "1.11.2",
+                "platform": "apo",
+                "inboundWebSocket": false,
+                "lastUpdated": 0,
+                "blacklisted": false,
+                "announcedAddress": "uk.apoacc.com",
+                "apiPort": 27876,
+                "application": "NRS",
+                "port": 27874,
+                "outboundWebSocket": true,
+                "lastConnectAttempt": 0,
+                "state": 1,
+                "shareAddress": true
+            },
+            {
+                "downloadedVolume": 491153,
+                "address": "us.apoacc.com",
+                "inbound": false,
+                "blockchainState": "UP_TO_DATE",
+                "weight": 0,
+                "uploadedVolume": 139210,
+                "services": [
+                    "API",
+                    "CORS"
+                ],
+                "version": "1.11.2",
+                "platform": "apo",
+                "inboundWebSocket": false,
+                "lastUpdated": 0,
+                "blacklisted": false,
+                "announcedAddress": "139.162.180.60",
+                "apiPort": 27876,
+                "application": "NRS",
+                "port": 27874,
+                "outboundWebSocket": true,
+                "lastConnectAttempt": 0,
+                "state": 1,
+                "shareAddress": true
+            }
+        ],
+        "requestProcessingTime": 5
+    };
